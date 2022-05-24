99aac2c5fdb764077ba47eeb4d0524d41531a369
Julia Klein <juliaklein@Julias-MacBook-Pro-3.local>
2022-05-24 16:09:49 +0200

    new histogram data plots for Isola

--- data/bees_morgane/create_hist.R
+++ data/bees_morgane/create_hist.R
@@ -51,6 +51,28 @@ grid.arrange(p1, p2, p3, p4, ncol=4, left = textGrob("Frequency", gp=gpar(fontsi
 dev.off()
 
 
+# create histogram like in Bees Paper
+dat <- read.table("hist1_PO.txt", sep=",", fill=TRUE)
+colony <- dat[1,]
+colony <- colony[!is.na(colony)]
+stinging <- dat[2:nrow(dat),]
+dataset1 <- data.frame(dataset = 1,
+                      group = c(rep("1 bee", 2), rep("2 bees", 3), 
+                                rep("5 bees", 6), rep("10 bees", 11)),
+                      stinging = as.factor(c(0, 1, 0:2, 0:5, 0:10)),
+                      frequency = c(as.numeric(na.omit(as.vector(t(stinging))))))
+dataset1$group <- factor(dataset1$group, levels=c('1 bee', '2 bees', '5 bees', 
+                                                '10 bees'))
+png("hist1PO_grid.png", width = 980, height = 250)
+ggplot(dataset1, aes(stinging, frequency, label = stinging, width = 0.75)) + 
+  geom_bar(stat="identity", fill="gray70", color="black") + 
+  facet_grid(~group, scales="free", space = "free_x") +
+  labs(x = " ", y = "Frequency") +
+  theme_bw() +
+  ggtitle("Dataset A") +
+  theme(plot.title = element_text(hjust = 0.5),
+        text = element_text(size = 20))
+dev.off()
 
 
 
@@ -89,6 +111,31 @@ png("hist1IAA_all.png", width = 12, height = 4, units = "in", res = 300)
 
 grid.arrange(p1, p2, p3, p4, ncol=4, left = textGrob("Frequency", gp=gpar(fontsize=12), vjust=1, rot=90),
              bottom = textGrob("Number of stinging bees", gp=gpar(fontsize=12), vjust=-1))
+dev.off()
+
+
+# create histogram like in Bees Paper
+dat <- read.table("hist1_IAA.txt", sep=",", fill=TRUE)
+colony <- dat[1,]
+colony <- colony[!is.na(colony)]
+stinging <- dat[2:nrow(dat),]
+dataset2 <- data.frame(dataset = 2,
+                       group = c(rep("1 bee", 2), rep("2 bees", 3), 
+                                rep("5 bees", 6), rep("10 bees", 11)),
+                      stinging = as.factor(c(0, 1, 0:2, 0:5, 0:10)),
+                      frequency = c(as.numeric(na.omit(as.vector(t(stinging))))))
+dataset2$group <- factor(dataset2$group, levels=c('1 bee', '2 bees', '5 bees', 
+                                                '10 bees'))
+
+png("hist1IAA_grid.png", width = 980, height = 250)
+ggplot(dataset2, aes(stinging, frequency, label = stinging, width = 0.75)) + 
+  geom_bar(stat="identity", fill="gray70", color="black") + 
+  facet_grid(~group, scales="free", space = "free_x") +
+  labs(x = " ", y = "Frequency") +
+  theme_bw() +
+  ggtitle("Dataset B") +
+  theme(plot.title = element_text(hjust = 0.5),
+        text = element_text(size = 20))
 dev.off()
 
 
@@ -136,4 +183,41 @@ png("hist2_all.png", width = 12, height = 4, units = "in", res = 300)
 
 grid.arrange(p1, p2, p3, p4, p5, p6, ncol=6, left = textGrob("Frequency", gp=gpar(fontsize=12), vjust=1, rot=90),
              bottom = textGrob("Number of stinging bees", gp=gpar(fontsize=12), vjust=-1))
+dev.off()
+
+# create histogram like in Bees Paper
+dat <- read.table("hist2.txt", sep=",", fill=TRUE)
+colony <- dat[1,]
+colony <- colony[!is.na(colony)]
+stinging <- dat[2:nrow(dat),]
+dataset3 <- data.frame(dataset = 3,
+                       group = c(rep("1 bee", 2), rep("2 bees", 3), 
+                                rep("5 bees", 6), rep("7 bees", 8), 
+                                rep("10 bees", 11), rep("15 bees", 16)),
+                      stinging = as.factor(c(0, 1, 0:2, 0:5, 0:7, 0:10, 0:15)),
+                      frequency = c(as.numeric(na.omit(as.vector(t(stinging))))))
+
+dataset3$group <- factor(dataset3$group, levels=c('1 bee', '2 bees', '5 bees', 
+                                                '7 bees', '10 bees', '15 bees'))
+png("hist2_grid.png", width = 980, height = 250)
+ggplot(dataset3, aes(stinging, frequency, label = stinging, width = 0.75)) + 
+  geom_bar(stat="identity", fill="gray70", color="black") + 
+  facet_grid(~group, scales="free", space = "free_x") +
+  labs(x = "Number of bees stinging", y = "Frequency") +
+  theme_bw() +
+  ggtitle("Dataset C") +
+  theme(plot.title = element_text(hjust = 0.5),
+        text = element_text(size = 20))
+dev.off()
+
+dataset <- rbind(dataset1, dataset2, dataset3)
+
+#png("hist1IAA_grid.png", width = 980, height = 250)
+ggplot(dataset, aes(stinging, frequency, label = stinging, width = 0.75)) + 
+  geom_bar(stat="identity", fill = "gray70", color = "black") + 
+  #facet_grid(dataset~group, scales="free", space = "free_x") +
+  facet_wrap(dataset~group, ncol = 4, dir = "h", scales = "free_x") +
+  labs(x = "Number of bees stinging", y = "Frequency of observation") +
+  theme_bw() +
+  theme(text = element_text(size = 20))
 dev.off()
